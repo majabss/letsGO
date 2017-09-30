2ddc62884cfd67940033be5ea85f01473463b88d
Jonas <jonasrieskamp@googlemail.com>
2017-09-30 11:55:02 +0200

    Home Seite mit der Liste der Spiele, dem plus Button and so on... (nur Oberfläche)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public player = 'Spieler 1';
+  public scoreWeek = 500;
+  public scoreAlltime = 1000;
 
-  }
+  public opponentName = 'Lars';
+  public opponentScore = 5000;
+  public gameStatus = 'blabla';
+
+  constructor(public navCtrl: NavController) { }
 
 }
