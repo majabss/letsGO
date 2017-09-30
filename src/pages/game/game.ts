f18ae31dad611d8bd49998d9c2c0f927ac756a86
Jonas <jonasrieskamp@googlemail.com>
2017-09-30 15:50:57 +0200

    Game Page

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PlayerTile } from '../../interfaces';
 
 /**
  * Generated class for the GamePage page.
@@ -15,11 +16,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GamePage {
 
+  public board: Array<PlayerTile[]>;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.initBoard(13);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GamePage');
+  }
+
+  public initBoard(boardSize: number) {
+    // this.board = new Array();
+    // for (let i = 0; i < boardSize; i++) {
+    //   this.board[i] = new Array<PlayerTile>(boardSize);
+    //   for (var j = 0; i < boardSize; i++) {
+    //     this.board[i][j] = PlayerTile.FREE;
+    //   }
+    // }
+
+    this.board = [
+      [PlayerTile.FREE, PlayerTile.FREE, PlayerTile.FREE],
+      [PlayerTile.FREE, PlayerTile.FREE, PlayerTile.FREE],
+      [PlayerTile.FREE, PlayerTile.FREE, PlayerTile.FREE],
+      [PlayerTile.FREE, PlayerTile.FREE, PlayerTile.FREE]
+    ];
+    console.log('board', this.board);
   }
 
 }
